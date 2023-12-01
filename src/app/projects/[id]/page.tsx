a985aa204c064e883f645207398cd81071d8efb0
Levi Barker <levi.barker@outlook.com>
2023-12-01 12:18:04 -0600

    added youtube videos

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -9,6 +9,7 @@ import {
 } from "firebase/firestore";
 import Link from "next/link";
 import { Project as ProjectModel } from "@/app/models/project";
+import ProjectMetadata from "@/app/ui/ProjectMetadata";
 
 const firestore = getFirestore(app);
 
@@ -24,20 +25,33 @@ export default async function Project({ params }: { params: { id: string } }) {
   return (
     <main className="h-screen overflow-auto bg-slate-200">
       <Link href="/">Go Back</Link>
-      <section className="flex flex-col justify-center items-start">
-        <h1 className="text-xl font-bold text-red-600">{project.title}</h1>
-        <span>Video URL: {project.video_url}</span>
-        <span>Thumbnail URL: {project.thumbnail_url}</span>
-        <div className="p-4 rounded-2xl bg-yellow-100/90 text-yellow-800">
-          <h3 className="font-bold">Supplies</h3>
-          <ul>
-            {project.supplies.map((supply) => (
-              <li key={supply.name}>
-                {supply.quantity} - {supply.name}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <section className="flex justify-center items-center">
+        <article className="max-w-lg w-full flex flex-col gap-4 p-4">
+          <h1 className="text-3xl font-bold text-red-600">{project.title}</h1>
+          <iframe
+            height="315"
+            src={project.video_url}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <div className="bg-slate-700 p-2 rounded-2xl w-full">
+            <ProjectMetadata {...project} />
+          </div>
+          <div className="p-4 rounded-2xl bg-yellow-100/90 text-yellow-800">
+            <h3 className="font-bold">Supplies</h3>
+            <ul>
+              {project.supplies.map((supply) => (
+                <li key={supply.name}>
+                  {supply.quantity} - {supply.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <span className="text-teal-800 font-bold text-2xl">Step 1</span>
+          <span className="text-teal-800 font-bold text-2xl">Step 2</span>
+        </article>
       </section>
     </main>
   );
